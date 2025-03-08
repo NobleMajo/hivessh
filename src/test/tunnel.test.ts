559b753689fc1c4ca666b50e0fe341dd1006b713
root <39386799+NobleMajo@users.noreply.github.com>
2025-03-08 19:13:18 +0000

    Add: port tunnel out tests

--- src/test/tunnel.test.ts
+++ src/test/tunnel.test.ts
@@ -13,64 +13,89 @@ const testSshHosts: [TestHost, SshHost][] = await connectTestHosts(hostData)
 
 export async function tunnelDockerToPort(
     test: TestHost,
-    sshHost: SshHost
+    sshHost: SshHost,
+    port: number,
 ): Promise<any> {
-    console.info("Test: ", test)
+    let server: Server | undefined = undefined
 
-    const server: Server = await sshHost.tunnelOut({
-        localHost: "127.0.0.1",
-        localPort: 54321,
-        remotePath: "/var/run/docker.sock",
-    })
+    try {
+        server = await sshHost.tunnelOut({
+            localHost: "127.0.0.1",
+            localPort: port,
+            remotePath: "/var/run/docker.sock",
+        })
 
-    const resp = await fetch(
-        "http://127.0.0.1:54321/containers/json",
-        {
-            method: "GET",
+        const resp = await fetch(
+            "http://127.0.0.1:" + port + "/containers/json",
+            {
+                method: "GET",
+            }
+        )
+
+        if (resp.status !== 200) {
+            throw new Error("Failed to fetch docker containers")
         }
-    )
 
-    if (resp.status !== 200) {
-        throw new Error("Failed to fetch docker containers")
+        const json = await resp.json() as any
+        if (!Array.isArray(json)) {
+            console.error("Invalid json response: ", json)
+            return
+        }
+        console.log("Response: ", json.length)
+    } finally {
+        if (server) {
+            server.close()
+        }
     }
-
-    console.log("Response: ", await resp.text())
-
-    server.close()
 }
 
 export async function tunnelDockerToSock(
     test: TestHost,
     sshHost: SshHost
 ): Promise<any> {
-    console.info("Test: ", test)
+    let server: Server | undefined = undefined
 
-    const server: Server = await sshHost.tunnelOut({
-        localPath: "/tmp/hivessh-test-" + test.id + ".sock",
-        remotePath: "/var/run/docker.sock",
-    })
-
-    const resp = await fetch(
-        "http://localhost/containers/json", {
-        dispatcher: new Agent({
-            connect: {
-                socketPath: "/tmp/hivessh-test-" + test.id + ".sock"
-            }
+    try {
+        server = await sshHost.tunnelOut({
+            localPath: "/tmp/hivessh-test-" + test.id + ".sock",
+            remotePath: "/var/run/docker.sock",
         })
-    })
 
-    if (resp.status !== 200) {
-        throw new Error("Failed to fetch docker containers, status: " + resp.status)
+        const resp = await fetch(
+            "http://localhost/containers/json", {
+            dispatcher: new Agent({
+                connect: {
+                    socketPath: "/tmp/hivessh-test-" + test.id + ".sock"
+                }
+            })
+        })
+
+        if (resp.status !== 200) {
+            throw new Error("Failed to fetch docker containers, status: " + resp.status)
+        }
+
+        const json = await resp.json() as any
+        if (!Array.isArray(json)) {
+            console.error("Invalid json response: ", json)
+            return
+        }
+        console.log("Response: ", json.length)
+    } finally {
+        if (server) {
+            server.close()
+        }
     }
-
-    console.log("Response: ", await resp.text())
-
-    server.close()
 }
 
 console.info("Test tunneling...")
 for (const testHosts of testSshHosts) {
     await tunnelDockerToSock(testHosts[0], testHosts[1])
+}
+
+console.info("Test ports...")
+let i = 0
+for (const testHosts of testSshHosts) {
+    await tunnelDockerToPort(testHosts[0], testHosts[1], 54321 + i++)
 }
 
 console.info("Disconnect test hosts...")
